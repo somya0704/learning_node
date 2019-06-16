bbbc876410d4881f5642c7f8c80d2b7ab00db7c9
somya <somya.singhal0797@gmail.com>
2019-06-16 21:47:58 +0530

    constrcutor rexport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const learn = require('./learn');
-
+const constrcutors = require('./constructors');
 
 console.log(learn(2,4));
+console.log(new constrcutors.SomeMathObj());
---
+++ constructors.js
@@ -0,0 +1,8 @@
+
+class SomeMathObj{
+    constructor(){
+        console.log("object created");
+    }
+}
+
+module.exports.SomeMathObj = SomeMathObj;
